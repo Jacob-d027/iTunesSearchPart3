8d6d487a11050bca7bbe0a21839dca89047ed1af
Jacob Davis <jacobdofficial@gmail.com>
2023-12-04 10:07:10 -0700

    Initial Commit

---
+++ iTunesSearch/StoreItemListTableViewController.swift
@@ -0,0 +1,120 @@
+
+import UIKit
+
+@MainActor
+class StoreItemListTableViewController: UITableViewController {
+    
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var filterSegmentedControl: UISegmentedControl!
+    
+    // add item controller property
+    var storeItemController = StoreItemController()
+    
+    var items = [StoreItem]()
+    var imageLoadTasks: [IndexPath: Task<Void, Never>] = [:]
+    
+    let queryOptions = ["movie", "music", "software", "ebook"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func fetchMatchingItems() {
+        
+        self.items = []
+        self.tableView.reloadData()
+        
+        let searchTerm = searchBar.text ?? ""
+        let mediaType = queryOptions[filterSegmentedControl.selectedSegmentIndex]
+        
+        if !searchTerm.isEmpty {
+            
+            // set up query dictionary
+            let queryDictionary: [String: String] = [
+                "term": searchTerm,
+                "media": mediaType,
+                "lang": "en_us",
+                "limit": "10"
+            ]
+            // use the item controller to fetch items
+            Task {
+                do {
+                    let searchItems = try await storeItemController.fetchItems(matching: queryDictionary)
+                    items = searchItems
+                    tableView.reloadData()
+                } catch {
+                    print("An error occurred: \(error.localizedDescription)")
+                }
+            }
+            
+            // if successful, use the main queue to set self.items and reload the table view
+            // otherwise, print an error to the console
+        }
+    }
+    
+    func configure(cell: ItemCell, forItemAt indexPath: IndexPath) {
+        
+        let item = items[indexPath.row]
+        
+        // set cell.name to the item's name
+        cell.name = item.name
+        // set cell.artist to the item's artist
+        cell.artist = item.artist
+        // set cell.artworkImage to nil
+        cell.artworkImage = nil
+        
+        Task {
+            do {
+                let (imageData, _) = try await URLSession.shared.data(from: item.artworkURL)
+                cell.artworkImage = UIImage(data: imageData)
+                
+            } catch {
+                print("There was an error: \(error)")
+                
+            }
+        }
+    }
+    
+    @IBAction func filterOptionUpdated(_ sender: UISegmentedControl) {
+        
+        fetchMatchingItems()
+    }
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath) as! ItemCell
+        configure(cell: cell, forItemAt: indexPath)
+
+        return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        // cancel the image fetching task if we no longer need it
+        imageLoadTasks[indexPath]?.cancel()
+    }
+}
+
+extension StoreItemListTableViewController: UISearchBarDelegate {
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        fetchMatchingItems()
+        searchBar.resignFirstResponder()
+    }
+}
+
